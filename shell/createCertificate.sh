a2959c1520977fd5379bd64611e07d96609cc470
renataMelo88 <renata.melo@bandtec.com>
2022-05-24 12:30:43 -0300

    atualizando modelos de scripts

---
+++ shell/createCertificate.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "ASSISTENTE DE INSTALAÇÃO SSL"
+
+menu(){
+echo "====== Opções de uso: ======"
+echo ""
+echo "1. UPDATE DE CERTIFICADO ANTIGO"
+echo "2. RENOVAÇÃO DE CERTIFICADO JÁ EXISTENTE"
+echo ""
+echo "============================"
+
+
+case $opcao in
+1) echo "=== ATUALIZAÇÃO DO CERTIFICADO ANTIGO  ==="
+ # Se o certificado for um update do certificado antigo
+ 
+
+ #Após isso rodar o nginx-update, com o comando abaixo:
+ 
+
+
+2) echo "=== RENOVAÇÃO DE CERTIFICADO JÁ EXISTENTE ==="
+
+  #Renovação de certificado já existente, que não há uma atualização, é somente uma renovação mesmo, rode o comando abaixo:
+ 
+
+  #Após isso rodar o nginx-update, com o comando abaixo:
+  
+
+menu
